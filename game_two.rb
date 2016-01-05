8c63daa9d4ded7776f38f56ac85c197a56a3293a
Liam Sean Brady <liamseanbrady@gmail.com>
2016-01-05 14:05:50 -0500

    Separate comments. Show dynamically whether a ship has been sunk or not

--- game_two.rb
+++ game_two.rb
@@ -28,7 +28,7 @@ def display_board
   5 | #{@player_board[4][0]} | #{@player_board[4][1]} | #{@player_board[4][2]} | #{@player_board[4][3]} | #{@player_board[4][4]} |
     +---+---+---+---+---+
 
-  Destroyer: Alive    Cruiser: Alive    Battleship: Alive
+  Destroyer: #{@player_destroyer[:status]}    Cruiser: #{@player_cruiser[:status]}    Battleship: #{@player_battleship[:status]}
 
   R2D2's Board:
       1   2   3   4   5
@@ -44,127 +44,129 @@ def display_board
   5 | #{@computer_board[4][0]} | #{@computer_board[4][1]} | #{@computer_board[4][2]} | #{@computer_board[4][3]} | #{@computer_board[4][4]} |
     +---+---+---+---+---+
 
-  Destroyer: Alive    Cruiser: Alive    Battleship: Alive
+  Destroyer: #{@computer_destroyer[:status]}    Cruiser: #{@computer_cruiser[:status]}    Battleship: #{@computer_battleship[:status]}
 
   Please select a square (such as 3, 5) to open fire!  
   BOARD
 end
 
-# Ship
-{ coordinates: [[1, 2], [1, 3], [1, 4]], alive: true, spaces_taken: [[1, 3], [1, 4]] }
+player_ships = []
+@player_battleship = { coordinates: [[1, 2], [1, 3], [1, 4]], status: "Alive", spaces_taken: [] }
+@player_cruiser = { coordinates: [[1, 5], [2, 5]], status: "Alive", spaces_taken: [] }
+@player_destroyer = { coordinates: [1, 5], status: "Alive" }
 
-# Player is randomly allocated 3 ships (1 destroyer, 1 cruiser, 1 battleship)
-  # Create player's ships array
-    player_ships = []
-  # Create battleship and put in ships array
-    player_battleship = { coordinates: [[1, 2], [1, 3], [1, 4]], alive: true, spaces_taken: [] }
-  # Create cruiser and put in ships array
-    player_cruiser = { coordinates: [[1, 5], [2, 5]], alive: true, spaces_taken: [] }
-  # Create destroyer and put in ships array
-    player_destroyer = { coordinates: [1, 5], alive: true }
+computer_ships = []
+@computer_battleship = { coordinates: [[1, 2], [1, 3], [1, 4]], status: "Alive", spaces_taken: [] }
+@computer_cruiser = { coordinates: [[1, 5], [2, 5]], status: "Alive", spaces_taken: [] }
+@computer_destroyer = { coordinates: [1, 5], status: "Alive" }
+  
+@player_board = []
+5.times { @player_board << [" ", " ", " ", " ", " "] }
 
-# Computer is randomly allocated 3 ships (1 destroyer, 1 cruiser, 1 battleship)
-  # Create player's ships array
-    computer_ships = []
-  # Create battleship and put in ships array
-    computer_battleship = { coordinates: [[1, 2], [1, 3], [1, 4]], alive: true, spaces_taken: [] }
-  # Create cruiser and put in ships array
-    computer_cruiser = { coordinates: [[1, 5], [2, 5]], alive: true, spaces_taken: [] }
-  # Create destroyer and put in ships array
-    computer_destroyer = { coordinates: [1, 5], alive: true }
-    
-# Player is given a blank board
-  @player_board = []
-  5.times { @player_board << [" ", " ", " ", " ", " "] }
+@computer_board = []
+5.times { @computer_board << [" ", " ", " ", " ", " "] }
 
-# Computer is given a blank board
-  @computer_board = []
-  5.times { @computer_board << [" ", " ", " ", " ", " "] }
+computer_possible_moves = (1..5).to_a.product((1..5).to_a)
 
-  display_board
+display_board
 
-# Loop
-  loop do
-  # Player picks a square using coordinates (e.g., 3, 4)
-    player_guess = gets.chomp.split(",").map(&:strip).map(&:to_i)
-    # If square is occupied by one of the coordinates of a battleship, then an X is placed in that square
-      if computer_battleship[:coordinates].include?(player_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        computer_battleship[:spaces_taken] << player_guess
-      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
-        if computer_battleship[:spaces_taken].size == 3
-          computer_battleship[:alive] = false
-        end
-      # put X in player_board in correct position
-        @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
-      end
-    # If square is occupied by one of the coordinates of a cruiser, then an X is placed in that square
-      if computer_cruiser[:coordinates].include?(player_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        computer_cruiser[:spaces_taken] << player_guess
-      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
-        if computer_cruiser[:spaces_taken].size == 2
-          computer_cruiser[:alive] = false
-        end
-      # put X in player_board in correct position
-        @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
-      end
-    # If square is occupied by one of the coordinates of a destroyer, then an X is placed in that square
-      if computer_destroyer[:coordinates].include?(player_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        computer_destroyer[:spaces_taken] << player_guess
-      # Change the status of that boat to Sunk
-        computer_destroyer[:alive] = false
-      # put X in player_board in correct position
-        @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
-      end
+loop do
+  player_guess = gets.chomp.split(",").map(&:strip).map(&:to_i)
 
-    # Else, a / is placed in that square
-      if !computer_destroyer[:coordinates].include?(player_guess) && !computer_cruiser[:coordinates].include?(player_guess) && !computer_battleship[:coordinates].include?(player_guess) 
-        @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "/"
-      end
-    # Has the player won?
-
-  # Computer picks a square using coordinates (e.g., 3, 4)
-    computer_guess = [[1, 1], [2, 1], [3, 1], [4, 5], [5, 1], [5, 2], [2, 4]].shuffle.shift
-    # If square is occupied by one of the coordinates of a battleship, then an X is placed in that square
-      if player_battleship[:coordinates].include?(computer_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        player_battleship[:spaces_taken] << computer_guess
-      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
-        if player_battleship[:spaces_taken].size == 3
-          player_battleship[:alive] = false
-        end
-      # put X in player_board in correct position
-        @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
-      end
-    # If square is occupied by one of the coordinates of a cruiser, then an X is placed in that square
-      if player_cruiser[:coordinates].include?(computer_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        player_cruiser[:spaces_taken] << computer_guess
-      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
-        if player_cruiser[:spaces_taken].size == 2
-          player_cruiser[:alive] = false
-        end
-      # put X in player_board in correct position
-        @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
-      end
-    # If square is occupied by one of the coordinates of a destroyer, then an X is placed in that square
-      if player_destroyer[:coordinates].include?(computer_guess)
-      # Keep a record of which coordinates of a boat have been occupied and which are still free
-        player_destroyer[:spaces_taken] << computer
-      # Change the status of that boat to Sunk
-        player_destroyer[:alive] = false
-      # put X in player_board in correct position
-        @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
-      end
-
-    # Else, a / is placed in that square
-      if !player_destroyer[:coordinates].include?(computer_guess) && !player_cruiser[:coordinates].include?(computer_guess) && !player_battleship[:coordinates].include?(computer_guess) 
-        @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "/"
-      end
-
-    display_board
+  if @computer_battleship[:coordinates].include?(player_guess)
+    @computer_battleship[:spaces_taken] << player_guess
+    if @computer_battleship[:spaces_taken].size == 3
+      @computer_battleship[:status] = "Sunk"
+    end
+    @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
+  end
+  if @computer_cruiser[:coordinates].include?(player_guess)
+    @computer_cruiser[:spaces_taken] << player_guess
+    if @computer_cruiser[:spaces_taken].size == 2
+      @computer_cruiser[:status] = "Sunk"
+    end
+    @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
+  end
+  if @computer_destroyer[:coordinates].include?(player_guess)
+    @computer_destroyer[:spaces_taken] << player_guess
+    @computer_destroyer[:status] = "Sunk"
+    @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "X"
   end
 
-# Show the board
+  if !@computer_destroyer[:coordinates].include?(player_guess) && !@computer_cruiser[:coordinates].include?(player_guess) && !@computer_battleship[:coordinates].include?(player_guess) 
+    @computer_board[player_guess[0] - 1][player_guess[1] - 1] = "/"
+  end
+
+  computer_guess = computer_possible_moves.shuffle.shift
+  if @player_battleship[:coordinates].include?(computer_guess)
+    @player_battleship[:spaces_taken] << computer_guess
+    if @player_battleship[:spaces_taken].size == 3
+      @player_battleship[:status] = "Sunk"
+    end
+    @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
+  end
+  if @player_cruiser[:coordinates].include?(computer_guess)
+    @player_cruiser[:spaces_taken] << computer_guess
+    if @player_cruiser[:spaces_taken].size == 2
+      @player_cruiser[:status] = "Sunk"
+    end
+    @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
+  end
+  if @player_destroyer[:coordinates].include?(computer_guess)
+    @player_destroyer[:spaces_taken] << computer
+    @player_destroyer[:status] = "Sunk"
+    @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "X"
+  end
+
+  if !@player_destroyer[:coordinates].include?(computer_guess) && !@player_cruiser[:coordinates].include?(computer_guess) && !@player_battleship[:coordinates].include?(computer_guess) 
+    @player_board[computer_guess[0] - 1][computer_guess[1] - 1] = "/"
+  end
+
+  display_board
+end
+
+
+# Ship
+# Player is randomly allocated 3 ships (1 destroyer, 1 cruiser, 1 battleship)
+  # Create player's ships array
+  # Create battleship and put in ships array
+  # Create cruiser and put in ships array
+  # Create destroyer and put in ships array
+# Computer is randomly allocated 3 ships (1 destroyer, 1 cruiser, 1 battleship)
+  # Create player's ships array
+  # Create battleship and put in ships array
+  # Create cruiser and put in ships array
+  # Create destroyer and put in ships array
+# Player is given a blank board
+# Computer is given a blank board
+# Loop
+  # Player picks a square using coordinates (e.g., 3, 4)
+    # If square is occupied by one of the coordinates of a battleship, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # If square is occupied by one of the coordinates of a cruiser, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # If square is occupied by one of the coordinates of a destroyer, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # Else, a / is placed in that square
+    # Has the player won?
+  # Computer picks a square using coordinates (e.g., 3, 4)
+    # If square is occupied by one of the coordinates of a battleship, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # If square is occupied by one of the coordinates of a cruiser, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Is the boat which was hit sunk now? If so, then change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # If square is occupied by one of the coordinates of a destroyer, then an X is placed in that square
+      # Keep a record of which coordinates of a boat have been occupied and which are still free
+      # Change the status of that boat to Sunk
+      # put X in player_board in correct position
+    # Else, a / is placed in that square
+    # Show the board
